dd8e71ff802f0d69cf0e36690f9c5b1a9baccc0f
Sjors <sjors.driessen@outlook.com>
2020-06-21 21:05:16 +0200

    Tweede push omdb api POC

--- IMDBapi/script.js
+++ IMDBapi/script.js
@@ -1,15 +1,19 @@
 function myFunction() {
-  var varfn = document.getElementById("searchText").value;
-  console.log(varfn);
+  let varfn = document.getElementById("searchText").value;
+  getMovies(varfn);
+  // console.log(varfn);
 
 
   // var varfn = document.getElementById("naam-film").value;
   // console.log(varfn);
 
+  function getMovies(varfn) {
+
+  }
 
   let apiData = {
     url: 'http://www.omdbapi.com/',
-    apiKey: '?apikey=623dcc99&t=',
+    apiKey: '?apikey=623dcc99&s=',
     filmnaam: varfn,
   }
 
@@ -20,19 +24,46 @@ function myFunction() {
   } = apiData
   const apiUrl = `${url}${apiKey}${filmnaam}`
 
+  let res;
+
   fetch(apiUrl)
-    .then((data) => data.json())
-    .then((poster) => createHtml(poster))
+    .then((response) => response.json())
+    .then(data => res = data)
+    .then((res) => {
 
-  const createHtml = (data) => {
-    console.log(data)
-    const html = `
-    <h2>${data.Title}</h2>
-    <div class="jaar">Year: ${data.Year}</div>
-    <img src=${data.Poster}>
-  `
+      let movies = res.Search;
+      let output = '';
 
-    const posterDiv = document.querySelector('.poster')
-    posterDiv.innerHTML = html
-  }
+      console.log(movies);
+      $.each(movies, (index, movie) => {
+        output += `
+        <div class ="col-md-3" id="ruimte">
+          <div class="well text-center">
+           <h5>${movie.Title}</h5>
+           <h6>Year: ${movie.Year}</h6>
+           <img src=${movie.Poster} onclick="movieSelected('${movie.imdbID}') href="#">
+           </div>
+         </div>
+        `;
+      });
+
+      $('#movies').html(output); //inner html kan mss ook?
+    });
+
+
+  //   .then((poster) => createHtml(poster))
+  //
+  // const createHtml = (data) => {
+  //   console.log(data)
+  //   const html = `
+  //   <div class ="col-md-3"?
+  //   <h2>${data.Title}</h2>
+  //   <div class="jaar">Year: ${data.Year}</div>
+  //   <img src=${data.Poster}>
+  //   </div>
+  // `
+  //
+  //   const posterDiv = document.querySelector('.poster')
+  //   posterDiv.innerHTML = html
 }
+// }
